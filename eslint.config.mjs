8d7313ec32e77097589feb47ed0e91f96c2bf437
SatrioHalim <satriohalim21@gmail.com>
2026-08-12 13:47:08 +0700

    First init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,14 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+// Custom Eslint
+eslintConfig.push({
+  rules:{
+    'react-hooks/exhaustive-deps': 'off',
+    'react/jsx-key': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+  }
+})
+
 export default eslintConfig;
